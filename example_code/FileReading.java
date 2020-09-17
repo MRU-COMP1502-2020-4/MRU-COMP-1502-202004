68adc8f224f9058b990cff5f371cc071b34fefa4
Tyson Kendon <tj@kendon.ca>
2020-09-17 12:08:14 -0600

    Added FileReading Example

---
+++ example_code/FileReading.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Scanner;
+
+public class FileReading {
+
+    public static void main(String[] args) {
+	
+	
+	// Every file needs a name, this is more complicated than just 
+	String filename = "notest.file";
+	
+	// File object allows us to play with files that are part of the file system 
+	File file = new File(filename);
+	
+	System.out.println(file.isFile());
+	
+	System.out.println(file.getAbsolutePath());
+	
+	try {
+	    // scanner is the "new" java file management tool
+	    Scanner scanner = new Scanner(file);
+	    // scanner can check if it has more lines to read from the file
+	    while (scanner.hasNextLine()) {
+		// and read them and we can print them with sysout
+		System.out.println(scanner.nextLine());
+		// you can also get scanner to look for the next word with .next()
+		// or you can get an int with .nextInt() or a double with .nextDouble()
+		// and so on.
+	    }
+	    scanner.close();
+	} catch (FileNotFoundException e) {
+	    // we can actually look at the stack trace to see all the method
+	    // java calls from the library.
+	    e.printStackTrace();
+	}
+    }
+
+}
