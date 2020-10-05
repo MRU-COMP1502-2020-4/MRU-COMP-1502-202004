5ac4b604c12bb7019d4d621f5bc9c6563d62fdcb
Tyson Kendon <tj@kendon.ca>
2020-10-05 16:52:58 -0600

    Added extra comment

--- counter_project/src/comp1502/peoplecounter/Counter.java
+++ counter_project/src/comp1502/peoplecounter/Counter.java
@@ -1,7 +1,17 @@
 package comp1502.peoplecounter;
 
+
+//import java.util.Scanner;
+
+// java package is a namespace
+// class has a fully qualified name, which is it's package name and its class name
+
 public class Counter {
 	
 	private int count;
+	
+	java.util.Scanner scanner; // fully qualified name
 
+	
+	//data
 }
