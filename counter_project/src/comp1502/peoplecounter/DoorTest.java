74137020c72c543d7d9ccbcb0115b7a22adc091e
Tyson Kendon <tj@kendon.ca>
2020-10-19 16:29:35 -0600

    Added DoorTest for testing Door

---
+++ counter_project/src/comp1502/peoplecounter/DoorTest.java
@@ -0,0 +1,14 @@
+package comp1502.peoplecounter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DoorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
