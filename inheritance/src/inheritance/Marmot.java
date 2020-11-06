6367af2d70ba1f994f9f1000af12390cd76dbba4
Tyson Kendon <tj@kendon.ca>
2020-11-06 14:04:13 -0700

    Added Marmot class to extend animal.

---
+++ inheritance/src/inheritance/Marmot.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class Marmot extends Animal {
+
+	public Marmot(String name) {
+		super(name);
+	}
+	
+	public void dig() {
+		System.out.println("Marmot dug");
+	}
+	
+	
+
+}
